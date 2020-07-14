55c19d5cb0ca8e1a858f716909adb0eda39f225a
srimans2000 <59472068+srimans2000@users.noreply.github.com>
2020-07-14 21:09:15 +0530

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
 from tests.test_planner import astar_test  # noqa: F401
 from tests.test_planner import prm_test  # noqa: F401
 from tests.test_planner import rrt_test  # noqa: F401
+from tests.test_planner import grassfire
