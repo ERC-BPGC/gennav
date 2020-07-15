aa18e5de7727973c1763e056de942d16cb030eb6
Srujan Deolasee <53463585+Srujan-D@users.noreply.github.com>
2020-07-15 22:32:46 +0530

    Update rrt.py
    1) Corrected <Node_obj>.position to <Node_obj>.state.position
    2) Added type of env (gennav.envs.Environment)
    3) Imported Environment to use get_traj_status method properly.

--- gennav/planners/rrt/rrt.py
+++ gennav/planners/rrt/rrt.py
@@ -1,11 +1,13 @@
 import math
 
-from gennav.envs import ScanEnv
+from gennav.envs import Environment
 from gennav.planners import Planner
 from gennav.utils import RobotState, Trajectory
 from gennav.utils.common import Node
 from gennav.utils.geometry import Point
 from gennav.utils.samplers import uniform_adjustable_random_sampler as sampler
+
+env_ = Environment()
 
 
 class RRT(Planner):
@@ -13,7 +15,7 @@ class RRT(Planner):
 
     Attributes:
         sample_area: area for sampling random points (min,max)
-        sampler: function to sample random points in sample_area
+        # sampler: function to sample random points in sample_area
         expand_dis: distance to expand tree by at each step
         goal_sample_rate: rate at which to sample goal during random sampling
     """
@@ -21,7 +23,6 @@ class RRT(Planner):
     def __init__(self, sample_area, expand_dis=0.1, goal_sample_rate=0.15):
         """Init RRT Parameters."""
 
-        RRT.__init__(self)
         self.sample_area = sample_area
         self.expand_dis = expand_dis
         self.goal_sample_rate = goal_sample_rate
@@ -32,7 +33,7 @@ class RRT(Planner):
         Args:
             start_point: Point (from gennav.utils.geometry) with start point coordinates.
             end_point: Point (from gennav.utils.geometry) with end point coordinates.
-            env: Base class for an envrionment.
+            env: (gennav.envs.Environment) Base class for an envrionment.
         Returns:
             A list of points (Point from gennav.utils.geometry) representing the path determined from
             start to goal while avoiding obstacles.
@@ -48,9 +49,9 @@ class RRT(Planner):
 
         # Calculate distances between start and goal
         del_x, del_y, del_z = (
-            start.position.x - goal_node.position.x,
-            start.position.y - goal_node.position.y,
-            start.position.z - goal_node.position.z,
+            start.state.position.x - goal_node.state.position.x,
+            start.state.position.y - goal_node.state.position.y,
+            start.state.position.z - goal_node.state.position.z,
         )
         distance_to_goal = math.sqrt(del_x ** 2 + del_y ** 2 + del_z ** 2)
 
@@ -58,7 +59,7 @@ class RRT(Planner):
         traj = Trajectory(
             [RobotState(position=start_point), RobotState(position=goal_point)]
         )
-        if not ScanEnv.get_traj_status(traj):
+        if not env_.get_traj_status(traj):
             while True:
                 # Sample random point in specified area
                 rnd_point = sampler(self.sample_area, goal_point, self.goal_sample_rate)
@@ -66,9 +67,9 @@ class RRT(Planner):
                 # Find nearest node to the sampled point
 
                 distance_list = [
-                    (node.position.x - rnd_point.x) ** 2
-                    + (node.position.y - rnd_point.y) ** 2
-                    + (node.position.z - rnd_point.z) ** 2
+                    (node.state.position.x - rnd_point.x) ** 2
+                    + (node.state.position.y - rnd_point.y) ** 2
+                    + (node.state.position.z - rnd_point.z) ** 2
                     for node in node_list
                 ]
 
@@ -86,20 +87,21 @@ class RRT(Planner):
                 # Create new point in the direction of sampled point
 
                 theta = math.atan2(
-                    rnd_point.y - nearest_node.position.y, rnd_point.x - nearest_node.x
+                    rnd_point.y - nearest_node.state.position.y,
+                    rnd_point.x - nearest_node.state.position.x,
                 )
 
                 new_point = Point()
 
-                new_point.x = nearest_node.position.x + self.expand_dis * math.cos(
-                    theta
+                new_point.x = (
+                    nearest_node.state.position.x + self.expand_dis * math.cos(theta)
                 )
                 new_point.y = (
-                    nearest_node.position.y + self.expand_dis * math.sin(theta),
+                    nearest_node.state.position.y + self.expand_dis * math.sin(theta),
                 )
 
                 # Check whether new point is inside an obstacles
-                if not ScanEnv.get_status(RobotState(position=new_point)):
+                if not env_.get_status(RobotState(position=new_point)):
                     new_point.x = float("nan")
                     new_point.y = float("nan")
                     continue
@@ -114,16 +116,16 @@ class RRT(Planner):
 
                 # Check if goal has been reached or if there is direct connection to goal
                 del_x, del_y, del_z = (
-                    new_node.position.x - goal_node.position.x,
-                    new_node.position.y - goal_node.position.y,
-                    new_node.position.z - goal_node.position.z,
+                    new_node.state.position.x - goal_node.state.position.x,
+                    new_node.state.position.y - goal_node.state.position.y,
+                    new_node.state.position.z - goal_node.state.position.z,
                 )
                 distance_to_goal = math.sqrt(del_x ** 2 + del_y ** 2 + del_z ** 2)
 
                 traj = Trajectory(
                     [RobotState(position=new_point), RobotState(position=goal_point)]
                 )
-                if distance_to_goal < self.expand_dis or ScanEnv.get_traj_status(traj):
+                if distance_to_goal < self.expand_dis or env_.get_traj_status(traj):
                     goal_node.parent = node_list[-1]
                     node_list.append(goal_node)
                     print("Goal reached!")
