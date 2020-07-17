31e0fd7d75c50d678ed27a55fb806ad73dcfc684
Vedant <vedantshah2012@gmail.com>
2020-07-17 12:18:12 +0530

    add minimum distance method to base class and polygon env

--- gennav/envs/polygon_env.py
+++ gennav/envs/polygon_env.py
@@ -94,7 +94,28 @@ class PolygonEnv(Environment):
 
         """
         point = Point(state.position.x, state.position.y, state.position.z)
-        nearest_obst = sorted(self.obstacle_list, key=lambda obj: point.distance(obj))[
+        nearest_obst = sorted(self.obstacle_list, key=lambda obj: point.distance(Polygon(obj)))[
             0
         ]
         return point.distance(nearest_obst), nearest_obst
+
+    def minimum_distances(self, state, sort = False):
+        """Function to get the minimum distance of each obstacle form the robot state
+
+        Uses shapely Point's distance method to obtain the minimum distances
+
+        Args:
+            state (gennav.utils.common.RobotState) : present state of the robot
+            sort (bool default = False) : returns the distances in ascending form when set true
+        Returns :
+            min_dist (list) : list containing minimum distances(float) of each obstacle from the robot state
+
+        """
+
+        point = Point(state.position.x, state.position.y, state.position.z)
+        min_dist = [point.distance(Polygon(obj)) for obj in obstacle_list]
+
+        if sort:
+            min_dist.sort()
+
+        return min_dist
