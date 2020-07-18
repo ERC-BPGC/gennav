5d36d45a47fc2d6f540f27697d676160d4c8daaf
Archit <rungtarchit1@gmail.com>
2020-07-18 23:39:42 +0530

    added prm*

---
+++ gennav/planners/prm/prmstar.py
@@ -0,0 +1,121 @@
+import math
+
+from gennav.planners.base import Planner
+from gennav.planners.graph_search.astar import astar
+from gennav.utils import RobotState, Trajectory
+
+
+class PRMSTAR(Planner):
+    """PRM-STAR Class.
+
+    Attributes:
+        sample_area (tuple): area for sampling random points (min,max)
+        sampler (function): function to sample random points in sample_area
+        c (float): a constant for radius determination
+        n (int): total no. of nodes to be sampled in sample_area
+    """
+
+    def __init__(self, sample_area, sampler, c, n):
+        """Init PRM Parameters."""
+
+        self.sample_area = sample_area
+        self.sampler = sampler
+        self.n = n
+        self.c = c
+
+    def construct(self, env):
+        """Constructs PRM-STAR graph.
+
+        Args:
+            env (gennav.envs.Environment): Base class for an envrionment.
+
+        Returns:
+            graph (dict): A dict where the keys correspond to nodes and
+                the values for each key is a list of the neighbour nodes
+        """
+        nodes = []
+        graph = {}
+        i = 0
+        # samples points from the sample space until n points
+        # outside obstacles are obtained
+        while i < self.n:
+            sample = self.sampler(self.sample_area)
+            if not env.get_status(RobotState(position=sample)):
+                continue
+            else:
+                i += 1
+                nodes.append(sample)
+
+        # finds neighbours for each node in a dynamic radius
+        for node1 in nodes:
+            for node2 in nodes:
+                if node1 != node2:
+                    r = self.c * math.sqrt(math.log(self.n) / self.n)
+                    dist = math.sqrt(
+                        (node1.x - node2.x) ** 2 + (node1.y - node2.y) ** 2
+                    )
+                    if dist < r:
+                        traj = Trajectory(
+                            [RobotState(position=node1), RobotState(position=node2)]
+                        )
+                        if env.get_traj_status(traj):
+                            if node1 not in graph:
+                                graph[node1] = [node2]
+                            elif node2 not in graph[node1]:
+                                graph[node1].append(node2)
+                            if node2 not in graph:
+                                graph[node2] = [node1]
+                            elif node1 not in graph[node2]:
+                                graph[node2].append(node1)
+
+        return graph
+
+    def plan(self, start, goal, env):
+        """Constructs a graph avoiding obstacles and then plans path from start to goal within the graph.
+
+        Args:
+            start (gennav.utils.RobotState): tuple with start point coordinates.
+            goal (gennav.utils.RobotState): tuple with end point coordinates.
+            env (gennav.envs.Environment): Base class for an envrionment.
+
+        Returns:
+            gennav.utils.Trajectory: The planned path as trajectory
+        """
+        # construct graph
+        graph = self.construct(env)
+        # find collision free point in graph closest to start_point
+        min_dist = float("inf")
+        for node in graph.keys():
+            dist = math.sqrt(
+                (node.x - start.position.x) ** 2 + (node.y - start.position.y) ** 2
+            )
+            traj = Trajectory(
+                [RobotState(position=node), RobotState(position=start.position)]
+            )
+            if dist < min_dist and (env.get_traj_status(traj)):
+                min_dist = dist
+                s = node
+        # find collision free point in graph closest to end_point
+        min_dist = float("inf")
+        for node in graph.keys():
+            dist = math.sqrt(
+                (node.x - goal.position.x) ** 2 + (node.y - goal.position.y) ** 2
+            )
+            traj = Trajectory(
+                [RobotState(position=node), RobotState(position=goal.position)]
+            )
+            if dist < min_dist and (env.get_traj_status(traj)):
+                min_dist = dist
+                e = node
+        # add start_point to path
+        path = [start]
+        traj = Trajectory(path)
+        # perform astar search
+        p = astar(graph, s, e)
+        if len(p.path) == 1:
+            return traj
+        else:
+            traj.path.extend(p.path)
+        # add end_point to path
+        traj.path.append(goal)
+        return traj
---
+++ tests/test_planners/prmstar_test.py
@@ -0,0 +1,49 @@
+from gennav.envs import PolygonEnv
+from gennav.planners.prm.prmstar import PRMSTAR
+from gennav.utils import RobotState
+from gennav.utils.geometry import Point
+from gennav.utils.samplers import uniform_random_sampler as sampler
+
+
+def test_prm_plan():
+    general_obstacles_list = [
+        [[(8, 5), (7, 8), (2, 9), (3, 5)], [(3, 3), (3, 5), (5, 5), (5, 3)]],
+        [
+            [(2, 10), (7, 10), (7, 1), (6, 1), (6, 6), (4, 6), (4, 9), (2, 9)],
+            [(4, 0), (4, 5), (5, 5), (5, 0)],
+            [(8, 2), (8, 7), (10, 7), (10, 2)],
+        ],
+    ]
+
+    poly = PolygonEnv()
+    for obstacles in general_obstacles_list:
+        poly.update(obstacles)
+
+        # Instatiate prm constructer object
+        start = RobotState(position=Point(0, 0))
+        goal = RobotState(position=Point(12, 10))
+        my_tree = PRMSTAR(sample_area=(-5, 15), sampler=sampler, c=30, n=100)
+        path = my_tree.plan(start, goal, poly)
+        # from gennav.envs.common import visualize_path
+        # visualize_path(path, poly)
+        assert poly.get_traj_status(path) is True
+
+
+def test_prm_construct():
+    general_obstacles_list = [
+        [[(8, 5), (7, 8), (2, 9), (3, 5)], [(3, 3), (3, 5), (5, 5), (5, 3)]],
+        [
+            [(2, 10), (7, 10), (7, 1), (6, 1), (6, 6), (4, 6), (4, 9), (2, 9)],
+            [(4, 0), (4, 5), (5, 5), (5, 0)],
+            [(8, 2), (8, 7), (10, 7), (10, 2)],
+        ],
+    ]
+    poly = PolygonEnv()
+    for obstacles in general_obstacles_list:
+        poly.update(obstacles)
+
+        # Instatiate prm constructer object
+        my_tree = PRMSTAR(sample_area=(-5, 15), sampler=sampler, c=30, n=50)
+        graph = my_tree.construct(poly)  # noqa: F841
+        # from gennav.utils.visualisation import visualize_graph
+        # visualize_graph(graph,poly)
