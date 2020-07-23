28e7ac3173a234648b532d82c212bf5f60841d7e
Atharv Sonwane <atharvs.twm@gmail.com>
2020-07-23 18:02:26 +0530

    created callable classes for samplers

--- gennav/utils/samplers.py
+++ gennav/utils/samplers.py
@@ -1,64 +1,104 @@
 import random
 from math import cos, pi, sin
 
+from gennav.utils.common import RobotState
 from gennav.utils.geometry import Point
 
 
-def uniform_adjustable_random_sampler(sample_area, goal, goal_sample_rate):
+class Sampler(object):
+    """Base classs for samplers.
     """
-    Randomly sample point in area while sampling goal point at a specified rate.
+
+    def __call___(self):
+        """Sample a configuration.
+
+        This method needs to be implemented in the specific sampler.
+
+        Returns:
+            gennav.utils.RobotState: The sampled robot state.
+        """
+        raise NotImplementedError
+
+
+class UniformRectSampler(Sampler):
+    """Uniformly sample point in given rectangular area while sampling goal point with
+        a specified probability.
 
     Args:
-        sample_area(tuple): area to sample point in (min and max)
-        goal(gennav.utils.geometry.Point):Point representing goal point.
-        goal_sample_rate: number between 0 and 1 specifying how often
-                            to sample the goal point.
+        min_x (float): Minimum x coordinate of sample area
+        min_x (float): Maximum x coordinate of sample area
+        min_x (float): Minimum y coordinate of sample area
+        min_x (float): Maximum y coordinate of sample area
+        goal (gennav.utils.RobotState): Goal state. Defaults to None
+        goal_sample_p (float): Probability of sampling goal. Defaults to 0
 
-    Return:
-        Randomly selected point as a Point(gennav.utils.geometry.Point).
+    Raises:
+        ValueError: If goal is not specified when goal_sample_p > 0
     """
 
-    if random.random() > goal_sample_rate:
-        new_point = Point()
-        new_point.x = random.uniform(sample_area[0], sample_area[1])
-        new_point.y = random.uniform(sample_area[0], sample_area[1])
-        return new_point
-    else:
-        return goal
+    def __init__(self, min_x, max_x, min_y, max_y, goal=None, goal_sample_p=0):
+        super(UniformRectSampler, self).__init__()
+        self.min_x = min_x
+        self.max_x = max_x
+        self.min_y = min_y
+        self.max_y = max_y
+        self.goal = goal
+        self.goal_sample_p = goal_sample_p
+        if goal is None and goal_sample_p > 0:
+            raise ValueError("goal much be specified if goal_sample_p > 0")
 
+    def __call__(self):
+        """
+        Randomly sample point in area while sampling goal point at a specified rate.
 
-def uniform_random_sampler(sample_area):
-    """
-    Randomly sample point in sample area
-
-    Args:
-        sample_area(tuple): area to sample point in (min and max)
-
-    Return:
-        Randomly selected point as a Point(gennav.utils.geometry.Point).
-    """
-    new_point = Point()
-    new_point.x = random.uniform(sample_area[0], sample_area[1])
-    new_point.y = random.uniform(sample_area[0], sample_area[1])
-    return new_point
-
-
-def uniform_random_circular_sampler(r):
-    """Randomly samples point in a circular region of radius r around the origin.
-        Args:
-            r: radius of the circle.
         Return:
-            Randomly selected point as Point(gennav.utils.geometry.Point).
-    """
-    # Generate a random angle theta.
-    theta = 2 * pi * random.random()
-    u = random.random() + random.random()
-    if u > 1:
-        a = 2 - u
-    else:
-        a = u
+            gennav.utils.RobotState: The sampled robot state.
+        """
+        if random.random() > self.goal_sample_p:
+            new_point = Point()
+            new_point.x = random.uniform(self.min_x, self.max_x)
+            new_point.y = random.uniform(self.min_y, self.max_y)
+            return RobotState(position=new_point)
+        else:
+            return self.goal
 
-    new_point = Point()
-    new_point.x = r * a * cos(theta)
-    new_point.y = r * a * sin(theta)
-    return new_point
+
+class UniformCircularSampler(Sampler):
+    """Uniformly sample point in given circular area while sampling goal point with
+        a specified probability.
+
+    Args:
+        radius (float): Radius of sample area
+        centre (gennav.utils.geometry.Point): Centre of circle to sample from.
+            Defaults to origin
+        goal (gennav.utils.RobotState): Goal state. Defaults to None
+        goal_sample_p (float): Probability of sampling goal. Defaults to 0
+
+    Raises:
+        ValueError: If goal is not specified when goal_sample_p > 0
+    """
+
+    def __init__(self, radius, centre=Point(), goal=None, goal_sample_p=0):
+        super(UniformCircularSampler, self).__init__()
+        self.centre = centre
+        self.r = radius
+        self.goal = goal
+        self.goal_sample_p = goal_sample_p
+        if goal is None and goal_sample_p > 0:
+            raise ValueError("goal much be specified if goal_sample_p > 0")
+
+    def __call__(self):
+        """Randomly sample point in area while sampling goal point at a specified rate.
+
+        Return:
+            gennav.utils.RobotState: The sampled robot state.
+        """
+        if random.random() > self.goal_sample_p:
+            theta = 2 * pi * random.random()
+            u = random.random() * self.r
+            new_point = Point()
+            new_point.x = self.centre.x + u * cos(theta)
+            new_point.y = self.centre.y + u * sin(theta)
+            return RobotState(position=new_point)
+        else:
+            return self.goal
