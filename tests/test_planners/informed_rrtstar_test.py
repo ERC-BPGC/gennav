9e63b902026889b1b4d2cf26b25bec9181941273
Sushant Sreeram <sushants12122000@gmail.com>
2020-07-24 18:35:45 +0530

    updated informed_rrtstar_test

--- tests/test_planners/informed_rrtstar_test.py
+++ tests/test_planners/informed_rrtstar_test.py
@@ -5,13 +5,22 @@ from gennav.utils.geometry import Point
 
 
 def test_informedrrtstar():
-    general_obstacles_list = [[(1, 1), (2, 1), (2, 2), (1, 2)], [(3,4), (4,4), (4,5), (3,5)]]
+    general_obstacles_list = [
+        [(1, 1), (2, 1), (2, 2), (1, 2)],
+        [(3, 4), (4, 4), (4, 5), (3, 5)],
+    ]
 
     poly = PolygonEnv()
     poly.update(general_obstacles_list)
-    
-   # Instantiate informed rrt* planner object
-    my_tree = InformedRRTstar(sample_area=(-2,13), expand_dis=0.1, neighbourhood_radius=0.5, goal_distance=0.5, max_iter=2000)
+
+    # Instantiate informed rrt* planner object
+    my_tree = InformedRRTstar(
+        sample_area=(-2, 13),
+        expand_dis=0.1,
+        neighbourhood_radius=0.5,
+        goal_distance=0.5,
+        max_iter=2000,
+    )
     start = RobotState(position=Point(1, 1))
     goal = RobotState(position=Point(10, 10))
     path = my_tree.plan(start, goal, poly)
