99627d82253d65480dc8cf37b8e2b9699e016c1b
Srujan Deolasee <53463585+Srujan-D@users.noreply.github.com>
2020-07-30 18:54:06 +0530

    Update rrg.py

--- gennav/planners/rrt/rrg.py
+++ gennav/planners/rrt/rrg.py
@@ -2,7 +2,7 @@ import math
 
 from gennav.planners import Planner
 from gennav.utils import RobotState, Trajectory
-from gennav.utils.geometry import Point
+from gennav.utils.geometry import Point, compute_distance
 from gennav.utils.graph import Graph
 from gennav.utils.graph_search.astar import astar
 
@@ -13,26 +13,23 @@ class RRG(Planner):
     """
 
     def __init__(
-        self, sample_area, sampler, expand_dis=1.0, goal_sample_rate=0.1, max_iter=1000,
+        self, sampler, goal_sample_p = 0.1, expand_dis=1.0, max_iter=500,
     ):
         """Init RRG parameters
 
         Args:
 
-            sample_area (tuple): area for sampling random points (min,max)
             sampler (function): samples random points in sample_area
+            goal_sample_p (float, optional): Probability of sampling goal. Defaults to 0.1
             expand_dis (float, optional): distance to expand tree by at each step. Defaults to 1.0.
-            goal_sample_rate (float, optional): rate at which to sample goal during random sampling. Defaults to 0.1.
             max_iter (int, optional): Maximum Iterations. Defaults to 500.
         """
-        self.sample_area = sample_area
         self.sampler = sampler
         self.exp_dis = expand_dis
-        self.goal_sample_rate = goal_sample_rate
         self.max_iter = max_iter
         self.graph = Graph()
         self.circle = 1.0
-
+        
     def plan(self, start, goal, env):
         """Plans path from start to goal avoiding obstacles.
         Args:
@@ -50,17 +47,14 @@ class RRG(Planner):
         # Loop for maximum iterations to get the best possible path
         for iter in range(self.max_iter):
 
-            rnd_point = self.sampler(self.sample_area)
-            rnd_node = RobotState(position=rnd_point)
+            rnd_node = self.sampler()
 
             # Find nearest node to the sampled point
 
             distance_list = [
-                (node.position.x - rnd_node.position.x) ** 2
-                + (node.position.y - rnd_node.position.y) ** 2
-                + (node.position.z - rnd_node.position.z) ** 2
-                for node in node_list
-            ]
+                    compute_distance(node.position, rnd_node.position)
+                    for node in node_list
+                ]
 
             try:
                 # for python2
@@ -94,23 +88,16 @@ class RRG(Planner):
             else:
                 node_list.append(new_node)
 
-            del_x, del_y, del_z = (
-                new_node.position.x - goal.position.x,
-                new_node.position.y - goal.position.y,
-                new_node.position.z - goal.position.z,
-            )
-            # dist_to_goal = math.sqrt(del_x ** 2 + del_y ** 2 + del_z ** 2)
+            # dist_to_goal = compute_distance(new_node.position, goal.position)
 
             # FINDING NEAREST INDICES
             nnode = len(node_list) + 1
             # The circle in which to check parent node and rewiring
             r = self.circle * math.sqrt((math.log(nnode) / nnode))
             dist_list = [
-                (rnd_node.position.x - new_node.position.x) ** 2
-                + (rnd_node.position.y - new_node.position.y) ** 2
-                + (rnd_node.position.z - new_node.position.z) ** 2
-                for node in node_list
-            ]
+                    compute_distance(node.position, new_node.position)
+                    for node in node_list
+                ]
             # Getting all the indexes within r units of new_node
             near_inds = [dist_list.index(i) for i in dist_list if i <= r ** 2]
             self.graph.add_node(new_node)
