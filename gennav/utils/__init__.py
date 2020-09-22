7fbe5316b5e0738515f61b550a6b6b8f2da2530d
veds12 <vedantshah2012@gmail.com>
2020-09-22 22:07:24 +0530

    fix linting error

--- gennav/utils/__init__.py
+++ gennav/utils/__init__.py
@@ -3,7 +3,7 @@ import gennav.utils.custom_exceptions  # noqa: F401
 import gennav.utils.geometry  # noqa: F401
 import gennav.utils.path_processing  # noqa: F401
 import gennav.utils.samplers  # noqa: F401
+from gennav.utils.common import Node  # noqa: F401
 from gennav.utils.common import RobotState  # noqa: F401
 from gennav.utils.common import Trajectory  # noqa: F401
 from gennav.utils.common import Velocity  # noqa: F401
-from gennav.utils.common import Node  # noqa: F401
