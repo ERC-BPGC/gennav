18ddd47889efdca8e654c77f3479e9c3df6e5cb3
Harshal Deshpande <hardesh1deshpande@gmail.com>
2020-10-24 17:26:07 +0530

    added rrt_connect

--- gennav/planners/rrt/__init__.py
+++ gennav/planners/rrt/__init__.py
@@ -1,3 +1,4 @@
 from gennav.planners.rrt.informed_rrtstar import InformedRRTstar  # noqa: F401
 from gennav.planners.rrt.rrg import RRG  # noqa: F401
 from gennav.planners.rrt.rrt import RRT  # noqa: F401
+from gennav.planners.rrt.rrt_connect import RRTConnect  # noqa: F401
